db0314e17bc23910d40ca25637d6e894f4b406bf
Joy Trinh <mongtuyen149@gmail.com>
2022-03-03 14:01:26 +0700

    create dynamic pages and link between pages

---
+++ pages/news/index.js
@@ -0,0 +1,16 @@
+import {Fragment} from 'react'
+import Link from 'next/link'
+
+function NewsPage ()
+{
+    return <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li>
+                <Link href='/news/nextjs-is-a-great-framework'>NextJS is a great framework</Link>
+            </li>
+            <li>Something else</li>
+        </ul>
+    </Fragment>
+}
+export default NewsPage
